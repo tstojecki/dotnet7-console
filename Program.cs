e2e9c3a679f539e61a93fbdf4a12ed86c48c1683
tstojecki <tstojecki@gmail.com>
2023-09-25 18:24:17 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+
+    public static async Task Main()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\data");
+        foreach (var file in Directory.EnumerateFiles(path))
+        {
+            Console.WriteLine(file);
+            Console.WriteLine(new FileInfo(file).FullName);
+        }
+    }
+}
